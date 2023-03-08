a28f79fb6e421280c4ed605bb94409f9a2c9a98f
Dulien <julien.martinet523@gmail.com>
2023-03-07 19:30:38 -0500

    email3 fixed

--- pages/test_1/test1_script.js
+++ pages/test_1/test1_script.js
@@ -12,7 +12,7 @@ function downloadCSV(array) {
     const url = URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
-    a.download = "data.csv";
+    a.download = "results.csv";
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
